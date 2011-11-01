f42a1c15569b6cd308be537816a457cdf8c2dd35
Dikang Gu <dikang85@gmail.com>
2011-11-01 17:32:52 +0800

    poj 1032

--- src/main/java/com/dikang/algorithm/poj/P1032.java
+++ src/main/java/com/dikang/algorithm/poj/P1032.java
@@ -19,30 +19,37 @@ public class P1032 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int i=2;
+        int i=1;
         int sum = 0;
 
-        while (sum + i <= N) {
+        while (sum + i + 1 <= N) {
+            i++;
             sum += i;
-            i ++;
         }
 
         int left = N - sum;
 
-        if (left == (i-2)) {
-            for (int j=2; j<i; j++) {
-                System.out.print((j+1) + " ");
+        if (left == i) {
+            for (int j=3; j<=i; j++) {
+                System.out.print(j + " ");
             }
+            System.out.println(i+2);
+        } else if (left == 0) {
+            int j = 2;
+            for (;j<i; j++)
+                System.out.print(j + " ");
+            System.out.println(i);
         } else {
             int j=2;
-            for (; j<i-left; j++) {
+            for (; j<=i-left; j++) {
                 System.out.print(j + " ");
             }
 
-            for (; j<i; j++) {
-                System.out.print((j+1) + " ");
+            j = i-left + 2;
+            for (; j<=i; j++) {
+                System.out.print(j + " ");
             }
+            System.out.println(i+1);
         }
-        System.out.println();
     }
 }
